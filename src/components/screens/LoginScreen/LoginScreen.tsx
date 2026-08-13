3b206d5b035158748567497fbd13658b500e68a4
ying <ying.li@entelect.co.za>
2026-08-13 11:42:20 +0200

    Scaffold login screen

---
+++ src/components/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+export const LoginScreen: FC = () => {
+  return (
+    <div className="flex flex-col h-dvh">
+      <main className="flex-1 min-h-0 overflow-y-auto">
+        <p>LOGIN SCREEN</p>
+      </main>
+    </div>
+  );
+};
